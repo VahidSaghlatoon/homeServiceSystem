b2dbee57aac9d1ad439295251b1babdc37b4ca7b
vahidSaghlatoon <vahid.d.w@gmail.com>
2022-03-03 20:31:57 +0330

    web security and web MVC config

---
+++ src/main/java/com/vahidsaghlatoon/homeservicesystem/config/UsersSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.vahidsaghlatoon.homeservicesystem.config;
+
+import com.vahidsaghlatoon.homeservicesystem.service.CustomerDetailsService;
+import com.vahidsaghlatoon.homeservicesystem.service.EmployeeDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Order(2)
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class UsersSecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final EmployeeDetailsService employeeDetailsService;
+    private final CustomerDetailsService customerDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder1() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(employeeDetailsService);
+        authProvider.setUserDetailsService(customerDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder1());
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+//        auth.userDetailsService(customerDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/register/**").permitAll()
+                .antMatchers("/adminLogin").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/employees/**").hasAuthority("مدیر")
+                .antMatchers("/mainServices/**").hasAuthority("مدیر")
+                .antMatchers("/customers/**").hasAuthority("مدیر")
+                .anyRequest()
+//                .permitAll();
+        //
+                .authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("email")
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout().permitAll()
+                .and().rememberMe().key("Abcdghgjgkkjgjhklhkl_6546546546")
+                .tokenValiditySeconds(7 * 24 * 60 * 60);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+
+                .antMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+}
